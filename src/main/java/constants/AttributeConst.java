c31f06d79343162cdc6385ecd7442da767f7be5f
shouta.suzuki13 <shota.suzuki0206@gmail.com>
2021-11-01 19:07:13 +0900

    「Employeeモデル、EmployeeViewモデル作成」

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -1,15 +1,15 @@
 package constants;
 
-/*
+/**
  * 画面の項目値等を定義するEnumクラス
+ *
  */
-
 public enum AttributeConst {
 
     //フラッシュメッセージ
     FLUSH("flush"),
 
-    //一覧画面共有
+    //一覧画面共通
     MAX_ROW("maxRow"),
     PAGE("page"),
 
@@ -26,7 +26,7 @@ public enum AttributeConst {
     //従業員管理
     EMPLOYEE("employee"),
     EMPLOYEES("employees"),
-    EMP_COUNT("employee_count"),
+    EMP_COUNT("employees_count"),
     EMP_ID("id"),
     EMP_CODE("code"),
     EMP_PASS("password"),
@@ -38,13 +38,13 @@ public enum AttributeConst {
     ROLE_GENERAL(0),
 
     //削除フラグ
-    DEL_FRAG_TRUE(1),
-    DEL_FRAG_FALSE(0),
+    DEL_FLAG_TRUE(1),
+    DEL_FLAG_FALSE(0),
 
     //日報管理
     REPORT("report"),
     REPORTS("reports"),
-    REP_COUNT("report_count"),
+    REP_COUNT("reports_count"),
     REP_ID("id"),
     REP_DATE("report_date"),
     REP_TITLE("title"),
@@ -70,4 +70,5 @@ public enum AttributeConst {
     public Integer getIntegerValue() {
         return this.i;
     }
+
 }
